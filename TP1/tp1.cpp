f73b85fb7b21b56e966bae78edafb584b4881578
Vico Heidbrink <vico-heidbrink@etud.univ-tln.fr>
2018-10-05 19:57:25 +0200

    TP1 done

--- TP1/tp1.cpp
+++ TP1/tp1.cpp
@@ -1,24 +1,17 @@
 #include "CImg.h"
+#include <iostream>
 using namespace cimg_library;
+using namespace std;
 
-//g++ -o tp1.exe tp1.cpp -O2 -L/usr/X11R6/lib -lm -lpthread -lX11
-
-int main(int argc, char** argv){
-
-	if(argc < 4){
-		printf("Usage: filename threshold p\n");
-		exit(1);
-	}
-
-	char* filename = argv[1];
-	int threshold = atoi(argv[2]);
-	int p = atoi(argv[3]);
+// Effectue la segmentation de l'image en fonction des paramètres passés en arguments
+CImg<float> segmentation(char* filename, int threshold, int p){
+	if(p < 3)
+		p = 3;
 
 	CImg<float> img;
-	float voxelSize[3];;
+	float voxelSize[3];
 
-	// On recupere les informations pratiques de l'image
-	img.load_analyze(argv[1], voxelSize);
+	img.load_analyze(filename, voxelSize);
 
 	// On applique le seuil
 	img.threshold(threshold);
@@ -32,14 +25,12 @@ int main(int argc, char** argv){
 	img.label();
 
 	int labels[2500];
-
 	for(int i=0; i<2500; i++){
 		labels[i] = 0;
 	}
 
 	int maxLabel = -1;
 	int maxOcc = -1;
-
 	// On cherche le label le plus present
 	for(int i=0; i<img.width()*img.height()*img.depth(); i++){
 		if(img[i] != 0){
@@ -67,5 +58,76 @@ int main(int argc, char** argv){
 	// On sauvegarde l'image resultante
 	img.save_analyze("Results/res.hdr", voxelSize);
 
+	return img;
+}
+
+/*
+	Les touches + et - du pavé numérique augmente et baisse la valeur du seuil
+	Les flèches du haut et du bas augmente et baisse la valeur de p, le nombre de transformations
+*/
+int main(int argc, char** argv){
+
+	if(argc < 4){
+		printf("Usage: filename threshold p\n");
+		exit(1);
+	}
+
+	char* filename = argv[1];
+	int threshold = atoi(argv[2]);
+	int p = atoi(argv[3]);
+
+	CImg<float> img = segmentation(filename, threshold, p);
+
+	CImgDisplay main_display(img, "Brain segmentation");
+    int slice_index = img.depth()/2;
+    CImg<> slice = img.get_slice(slice_index);   
+    main_display.display(slice);
+
+	while (!main_display.is_closed()) {
+		main_display.wait();
+        if(main_display.wheel()){
+        	// On change la coupe en fonction de l'action de la molette
+            int counter = main_display.wheel();
+            slice_index += counter;
+            if(slice_index < 0) 
+            	slice_index = 0;
+            if(slice_index >= img.depth()) 
+            	slice_index = img.depth()-1;
+
+            cout << "Affichage de la coupe " << slice_index+1 << endl;
+
+            // On affiche la nouvelle coupe
+            slice = img.get_slice(slice_index);
+            main_display.display(slice);
+
+            // On remet la molette à 0 pour faciliter sa lecture
+            main_display.set_wheel();
+        }
+        else if(main_display.is_keyPADADD()){
+        	threshold += 1;
+        }
+        else if(main_display.is_keyPADSUB()){
+        	threshold -= 1;
+        }
+        else if(main_display.is_keyARROWUP()){
+        	p += 1;
+        }
+        else if(main_display.is_keyARROWDOWN()){
+        	p -= 1;
+        }
+
+        // Dans ces cas là il faut mettre à jour le display
+        if(main_display.is_keyPADSUB() or main_display.is_keyPADADD() or main_display.is_keyARROWUP() or main_display.is_keyARROWDOWN()){
+        	cout << "Calcul en cours..." << endl;
+        	img = segmentation(filename, threshold, p);
+        	cout << "Nouvelle segmentation avec un seuil de " << threshold << " et " << p << " transformations." << endl;
+            // On affiche la nouvelle coupe
+            slice = img.get_slice(slice_index);
+            main_display.display(slice);
+        }
+	}
+
 	return 0;
 }
+
+//g++ -o tp1.exe tp1.cpp -O2 -L/usr/X11R6/lib -lm -lpthread -lX11
